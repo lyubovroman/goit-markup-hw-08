b695b8885cf071ff22a1fce265ca1129502b0b90
lyubovroman <lyubovromanou@gmail.com>
2022-11-25 21:54:41 +0200

    mobile-modal?

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,25 @@
 (() => {
-  const mobileMenu = document.querySelector('.data-menu-container');
-  const openMenuBtn = document.querySelector('.data-open-menu');
-  const closeMenuBtn = document.querySelector('.data-close-menu');
+  const mobileMenu = document.querySelector("[data-menu-container]");
+  const openMenuBtn = document.querySelector("[data-open-menu]");
+  const closeMenuBtn = document.querySelector("[data-close-menu]");
+  const wrapper = document.querySelector("body");
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+    const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    wrapper.classList.toggle("lock");
   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    wrapper.classList.remove("lock");
   });
 })();
